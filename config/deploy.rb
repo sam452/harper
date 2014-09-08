27599d1c76950ef94a49a42fc19c72dad264671f
sam walton <sam@foofrancine.com>
2014-09-07 19:33:26 -0500

    Harper solar

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,20 +10,57 @@ set :rack_env, :production
 
 set :user, 'root'
 set :domain, 'www.harpersolar.biz'
-set :applicationdir, "/var/www/harper/public/"
+set :applicationdir, "/var/www/harper/"
 set :scm, 'git'
 
 set :application, "harper"
-set :repository,  "set your repository location here"
+set :repository,  "git@github.com:sam452/harper.git"
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :deploy_via, :remote_cache
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+# set :deploy_to, applicationdir
+# set :deploy_via, :export
 
+ssh_options[:forward_agent] = true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+ssh_options[:paranoid] = false
+default_run_options[:pty] = true
+
+
+
+namespace :deploy do
+	desc "Not starting as we're running passenger"
+	task :start do
+  end
+
+  desc "Not stopping as we're running passenger"
+  task :stop do
+  end
+
+  desc "Restart the app"
+  task :restart, roles: :app, except: { :no_release => true } do
+  	run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+
+  task :cold do
+  	deploy.update
+  	deploy.start
+  end
+
+end
+
+after "deploy:cold" do
+	admin.nginx_restart
+end
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
